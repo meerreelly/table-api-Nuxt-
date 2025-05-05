dfb7e0668949176e12237f3b4fc53ed43b4f3151
merely <128153965+meerreelly@users.noreply.github.com>
2025-05-05 16:36:43 +0300

    fix: fixed table view

--- table-api/pages/index.vue
+++ table-api/pages/index.vue
@@ -1,217 +1,223 @@
 <script setup lang="ts">
-import { h, resolveComponent } from 'vue'
-import { getPaginationRowModel } from '@tanstack/vue-table'
-import type { TableColumn } from '@nuxt/ui'
-import type { Column } from '@tanstack/vue-table'
+import { h, resolveComponent } from "vue";
+import { getPaginationRowModel } from "@tanstack/vue-table";
+import type { TableColumn } from "@nuxt/ui";
+import type { Column } from "@tanstack/vue-table";
 
 useHead({
-  title: 'Список продуктів'
-})
+  title: "Список продуктів",
+});
 
-const UBadge = resolveComponent('UBadge')
-const UButton = resolveComponent('UButton')
-const UDropdownMenu = resolveComponent('UDropdownMenu')
-const UAvatar = resolveComponent('UAvatar')
+const UBadge = resolveComponent("UBadge");
+const UButton = resolveComponent("UButton");
+const UDropdownMenu = resolveComponent("UDropdownMenu");
+const UAvatar = resolveComponent("UAvatar");
 
-const table = useTemplateRef('table')
+const table = useTemplateRef("table");
 
 interface Product {
-  id: number
-  title: string
-  description: string
-  price: number
-  rating: number
-  brand: string
-  category: string
-  thumbnail: string
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
 }
 
 interface ProductsResponse {
-  products: Product[]
-  total: number
-  skip: number
-  limit: number
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
 }
 
-const { data: productsData, status } = await useFetch<ProductsResponse>('https://dummyjson.com/products?limit=0', {
-  key: 'products-table',
-  lazy: true
-})
+const { data: productsData, status } = await useFetch<ProductsResponse>(
+  "https://dummyjson.com/products?limit=0",
+  {
+    key: "products-table",
+    lazy: true,
+  }
+);
 
-
-const products = computed(() => productsData.value?.products || [])
+const products = computed(() => productsData.value?.products || []);
 
 function getHeaderWithoutSorting(label: string) {
   return h(UButton, {
-    color: 'neutral',
-    variant: 'ghost',
+    color: "neutral",
+    variant: "ghost",
     label,
-    class: '-mx-2.5',
-  })
+    class: "-mx-2.5",
+  });
 }
 
 function getHeader(column: Column<any>, label: string) {
-  const isSorted = column.getIsSorted()
+  const isSorted = column.getIsSorted();
 
   return h(
-      UDropdownMenu,
-      {
-        content: {
-          align: 'start'
-        },
-        'aria-label': 'Actions dropdown',
-        items: [
-          {
-            label: 'Asc',
-            type: 'checkbox',
-            icon: 'i-lucide-arrow-up-narrow-wide',
-            checked: isSorted === 'asc',
-            onSelect: () => {
-              if (isSorted === 'asc') {
-                column.clearSorting()
-              } else {
-                column.toggleSorting(false)
-              }
+    UDropdownMenu,
+    {
+      content: {
+        align: "start",
+      },
+      "aria-label": "Actions dropdown",
+      items: [
+        {
+          label: "Asc",
+          type: "checkbox",
+          icon: "i-lucide-arrow-up-narrow-wide",
+          checked: isSorted === "asc",
+          onSelect: () => {
+            if (isSorted === "asc") {
+              column.clearSorting();
+            } else {
+              column.toggleSorting(false);
             }
           },
-          {
-            label: 'Desc',
-            icon: 'i-lucide-arrow-down-wide-narrow',
-            type: 'checkbox',
-            checked: isSorted === 'desc',
-            onSelect: () => {
-              if (isSorted === 'desc') {
-                column.clearSorting()
-              } else {
-                column.toggleSorting(true)
-              }
+        },
+        {
+          label: "Desc",
+          icon: "i-lucide-arrow-down-wide-narrow",
+          type: "checkbox",
+          checked: isSorted === "desc",
+          onSelect: () => {
+            if (isSorted === "desc") {
+              column.clearSorting();
+            } else {
+              column.toggleSorting(true);
             }
-          }
-        ]
-      },
-      () =>
-          h(UButton, {
-            color: 'neutral',
-            variant: 'ghost',
-            label,
-            icon: isSorted
-                ? isSorted === 'asc'
-                    ? 'i-lucide-arrow-up-narrow-wide'
-                    : 'i-lucide-arrow-down-wide-narrow'
-                : 'i-lucide-arrow-up-down',
-            class: '-mx-2.5 data-[state=open]:bg-(--ui-bg-elevated)',
-            'aria-label': `Sort by ${isSorted === 'asc' ? 'descending' : 'ascending'}`
-          })
-  )
+          },
+        },
+      ],
+    },
+    () =>
+      h(UButton, {
+        color: "neutral",
+        variant: "ghost",
+        label,
+        icon: isSorted
+          ? isSorted === "asc"
+            ? "i-lucide-arrow-up-narrow-wide"
+            : "i-lucide-arrow-down-wide-narrow"
+          : "i-lucide-arrow-up-down",
+        class: "-mx-2.5 data-[state=open]:bg-(--ui-bg-elevated)",
+        "aria-label": `Sort by ${
+          isSorted === "asc" ? "descending" : "ascending"
+        }`,
+      })
+  );
 }
 
 const columns: TableColumn<Product>[] = [
   {
-    accessorKey: 'title',
-    header: ({ column }) => getHeader(column, 'Назва'),
+    accessorKey: "title",
+    header: ({ column }) => getHeader(column, "Назва"),
   },
   {
-    accessorKey: 'description',
-    header: ({ column }) => getHeader(column, 'Опис'),
+    accessorKey: "description",
+    header: ({ column }) => getHeader(column, "Опис"),
     cell: ({ row }) => {
-      const description = row.getValue('description') as string
-      return description.length > 50 ? `${description.substring(0, 50)}...` : description
-    }
+      const description = row.getValue("description") as string;
+      return description.length > 50
+        ? `${description.substring(0, 50)}...`
+        : description;
+    },
   },
   {
-    accessorKey: 'price',
-    header: ({ column }) => getHeader(column, 'Ціна'),
+    accessorKey: "price",
+    header: ({ column }) => getHeader(column, "Ціна"),
     cell: ({ row }) => {
-      const price = Number.parseFloat(row.getValue('price'))
-      return h('div', { class: 'text-right font-medium' }, `$${price}`)
-    }
+      const price = Number.parseFloat(row.getValue("price"));
+      return h("div", { class: "text-right font-medium" }, `$${price}`);
+    },
   },
   {
-    accessorKey: 'rating',
-    header: ({ column }) => getHeader(column, 'Оцінка'),
+    accessorKey: "rating",
+    header: ({ column }) => getHeader(column, "Оцінка"),
     cell: ({ row }) => {
-      const rating = Number.parseFloat(row.getValue('rating'))
-      const color = rating >= 4.5 ? 'text-green-500' : 'text-red-500'
-      return h('div', { class: `font-medium ${color}` }, rating.toFixed(1))
-    }
+      const rating = Number.parseFloat(row.getValue("rating"));
+      const color = rating >= 4.5 ? "text-green-500" : "text-red-500";
+      return h("div", { class: `font-medium ${color}` }, rating.toFixed(1));
+    },
   },
   {
-    accessorKey: 'brand',
-    header: ({ column }) => getHeader(column, 'Бренд'),
+    accessorKey: "brand",
+    header: ({ column }) => getHeader(column, "Бренд"),
   },
   {
-    accessorKey: 'category',
-    header: ({ column }) => getHeader(column, 'Категорія'),
+    accessorKey: "category",
+    header: ({ column }) => getHeader(column, "Категорія"),
   },
   {
-    accessorKey: 'thumbnail',
-    header: () => getHeaderWithoutSorting('Фото'),
+    accessorKey: "thumbnail",
+    header: () => getHeaderWithoutSorting("Фото"),
     cell: ({ row }) => {
-      const thumbnail = row.getValue('thumbnail') as string
+      const thumbnail = row.getValue("thumbnail") as string;
       return h(UAvatar, {
         src: thumbnail,
-        alt: `${row.getValue('title')} thumbnail`,
-        class: 'rounded-md object-cover w-[100px] h-[100px]'
-      })
-    }
+        alt: `${row.getValue("title")} thumbnail`,
+        class: "rounded-md object-cover w-[100px] h-[100px]",
+      });
+    },
   },
-]
+];
 
 const pagination = ref({
   pageIndex: 0,
-  pageSize: 10
-})
+  pageSize: 5,
+});
 
 const sorting = ref([
   {
-    id: 'title',
-    desc: false
-  }
-])
+    id: "title",
+    desc: false,
+  },
+]);
 
-const globalFilter = ref('45')
-
+const globalFilter = ref("");
 </script>
 
 <template>
-  <div class="min-h-screen bg-[#2e2e2e] p-8">
-    <div class=" mx-auto">
-      <div class="bg-gray-700 rounded-lg">
-          <div class="mb-4 py-3 flex justify-end">
-            <UInput
-                v-model="globalFilter"
-                class=" px-5 py-2  min-w-80   me-4"
-                placeholder="Пошук"
-            />
-          </div>
-
-          <UTable
-              ref="table"
-              v-model:pagination="pagination"
-              model:global-filter="globalFilter"
-              :columns="columns"
-              :data="products"
-              :loading="status === 'pending'"
-              search-placeholder="Пошук товарів..."
-              :pagination-options="{
-            getPaginationRowModel: getPaginationRowModel()
-          }"
-              sort-mode="all"
+  <div class="min-h-screen p-8">
+    <div class="mx-auto">
+      <div class="rounded-lg">
+        <div class="mb-4 py-3 flex justify-end">
+          <UInput
+            v-model="globalFilter"
+            class="px-5 py-2 min-w-80 me-4"
+            placeholder="Пошук"
           />
-      </div>
+        </div>
 
+        <UTable
+          ref="table"
+          v-model:pagination="pagination"
+          v-model:global-filter="globalFilter"
+          :columns="columns"
+          :data="products"
+          :loading="status === 'pending'"
+          search-placeholder="Пошук товарів..."
+          :pagination-options="{
+            getPaginationRowModel: getPaginationRowModel(),
+          }"
+          sort-mode="all"
+          class="bg-gray-700 rounded-lg"
+        />
+      </div>
 
       <div class="flex justify-center border-t border-(--ui-border) pt-4 mt-4">
         <UPagination
-            :default-page="(table?.tableApi?.getState().pagination.pageIndex || 0) + 1"
-            :items-per-page="table?.tableApi?.getState().pagination.pageSize"
-            :total="products.length"
-            @update:page="(p) => table?.tableApi?.setPageIndex(p - 1)"
+          :default-page="
+            (table?.tableApi?.getState().pagination.pageIndex || 0) + 1
+          "
+          :items-per-page="table?.tableApi?.getState().pagination.pageSize"
+          :total="table?.tableApi?.getFilteredRowModel().rows.length"
+          @update:page="(p) => table?.tableApi?.setPageIndex(p - 1)"
         />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
